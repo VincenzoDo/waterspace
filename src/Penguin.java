18f4d4e7feac4d3d4e9bcb64f1f5deba2443ebde
r.monnard <r.monnard@ddbea90b-85ed-395a-11f5-6ae587be2f69>
2012-12-06 11:37:22 +0000

    waterworld creatures

---
+++ src/Penguin.java
@@ -0,0 +1,16 @@
+package WaterWorld;
+
+import FusionCalssDiagram.IWorldElement;
+
+public class Penguin extends Creature1 {
+	private boolean sex;
+	private int sexCounter;
+
+	public void breed() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void move() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ src/Shark.java
@@ -0,0 +1,21 @@
+package WaterWorld;
+
+import FusionCalssDiagram.IWorldElement;
+
+public class Shark extends Creature1 {
+	private boolean sex;
+	private int sexCounter;
+	private int eatCounter;
+
+	public void breed() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void eat() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void move() {
+		throw new UnsupportedOperationException();
+	}
+}
