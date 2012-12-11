28aa6deb11e0fb6925c914d038ed4a87217ec1dd
pauline.boukhaled <pauline.boukhaled@ddbea90b-85ed-395a-11f5-6ae587be2f69>
2012-12-11 10:19:53 +0000

    - add class IHM - implement concreteCommand nextStep()

--- src/ui/nextStep.java
+++ src/ui/nextStep.java
@@ -1,7 +1,21 @@
 package ui;
 
+import waterspace.AbstractWorld;
+
 
 
 public class nextStep extends Command {
-	//public IHM instantiate;
+    // Receiver
+    AbstractWorld world;
+    
+    public nextStep(AbstractWorld w){
+        this.world = w;
+    }
+    
+    public void execute (){
+        world.nextStep();
+    }
+    
+    //public void undo(){
+    //}
 }
