2d390e0a1ab579179ef061c5c77fddcbfb71570a
tarek.amiri <tarek.amiri@ddbea90b-85ed-395a-11f5-6ae587be2f69>
2012-12-13 13:28:08 +0000

    Implémentation des paramètres de simulation

---
+++ trunk/src/waterspace/SimulParams.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package waterspace;
+
+/**
+ *
+ * @author Raiden
+ */
+public class SimulParams {
+    
+    private  int mapWidth = 50;
+    private  int mapHeight = 50;
+
+    public int getMapWidth() {
+        return mapWidth;
+    }
+
+    public void setMapWidth(int mapWidth) {
+        this.mapWidth = mapWidth;
+    }
+
+    public int getMapHeight() {
+        return mapHeight;
+    }
+
+    public void setMapHeight(int mapHeight) {
+        this.mapHeight = mapHeight;
+    }
+    
+    
+    
+}
