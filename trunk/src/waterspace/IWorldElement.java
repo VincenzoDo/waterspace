943fb3067368b3d857d1d2aceb991174df7efe65
tarek.amiri <tarek.amiri@ddbea90b-85ed-395a-11f5-6ae587be2f69>
2012-12-13 14:29:41 +0000

    Ajout d'un deuxième constructeur pour permettre la création de singleton (BlackHole)

--- trunk/src/waterspace/IWorldElement.java
+++ trunk/src/waterspace/IWorldElement.java
@@ -1,50 +1,67 @@
 package waterspace;
 
 //Tarek
-
 import javax.swing.ImageIcon;
 
 public class IWorldElement {
-  private Position     pos;
-  private int          speed;
-  private ImageIcon    img;
-  private ElementType  type;
 
-  public AbstractWorld world;
+    private Position pos;
+    private int speed;
+    private ImageIcon img;
+    private ElementType type;
+    public AbstractWorld world;
 
-  public IWorldElement(Position pos, int speed, ImageIcon img,
-      ElementType type) {
-    this.pos = pos;
-    this.speed = speed;
-    this.img = img;
-    this.type = type;
-  }
+    /**
+     * This constructor is only used by sigleton classes !
+     */
+    public IWorldElement() {
+    }
 
-  public Position getPosition() {
-    return pos;
-  }
+    /**
+     * Regular Constructor for IWorldElement
+     *
+     * @param pos Position of Element
+     * @param speed Speed of Element
+     * @param img Image to display for the Element
+     * @param type Element Type
+     */
+    public IWorldElement(Position pos, int speed, ImageIcon img,
+            ElementType type) {
+        this.pos = pos;
+        this.speed = speed;
+        this.img = img;
+        this.type = type;
+    }
 
-  public ElementType getType() {
-    return type;
-  }
+    public Position getPosition() {
+        return pos;
+    }
 
-  public int getSpeed() {
-    return speed;
-  }
+    public ElementType getType() {
+        return type;
+    }
 
-  public ImageIcon getImage() {
-    return img;
-  }
+    public int getSpeed() {
+        return speed;
+    }
 
-  /*
-   * Specialisation de WaterWorld
-  public void placeElement() {
-    throw new UnsupportedOperationException();
-  }
+    public ImageIcon getImage() {
+        return img;
+    }
 
+    public void setPos(Position pos) {
+        this.pos = pos;
+    }
 
-  public void updateCounters() {
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
 
-  }
-  */
+    public void setImg(ImageIcon img) {
+        this.img = img;
+    }
+
+    public void setType(ElementType type) {
+        this.type = type;
+    }
 }
