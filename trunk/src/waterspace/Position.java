a27e3f91659ee90417e18de133ce72518adddf46
tarek.amiri <tarek.amiri@ddbea90b-85ed-395a-11f5-6ae587be2f69>
2012-12-13 13:27:34 +0000

    Ajout des déplacements

--- trunk/src/waterspace/Position.java
+++ trunk/src/waterspace/Position.java
@@ -1,36 +1,76 @@
 package waterspace;
 
 public class Position {
-  
-  private int xCor;
-  private int yCor;
-  
-  public Position(int x, int y){
-    xCor = x;
-    yCor = y;
-  }
-  
-  public int getX(){
-    return xCor;
-  }
-  
-  public int getY(){
-    return yCor;
-  }
-  
-  public void setX(int x){
-    xCor = x;
-  }
-  
-  public void setY(int y){
-    yCor = y;
-  }
-  
-  public void setNewPosition(int x, int y){
-    
-    setX(x);
-    setY(y);
-    
-  }
 
+    private int xCor;
+    private int yCor;
+    SimulParams params;
+
+    public Position(int x, int y, SimulParams params) {
+        xCor = x;
+        yCor = y;
+        this.params = params;
+    }
+
+    public int getX() {
+        return xCor;
+    }
+
+    public int getY() {
+        return yCor;
+    }
+
+    public void setX(int x) {
+        if (x < 0) {
+            xCor = 0;
+        } else if (x > params.getMapWidth()) {
+            xCor = params.getMapWidth();
+        }
+    }
+
+    public void setY(int y) {
+
+        if (y < 0) {
+            yCor = 0;
+        } else if (y > params.getMapHeight()) {
+            yCor = params.getMapHeight();
+        }
+    }
+
+    public void setNewPosition(int x, int y) {
+
+        setX(x);
+        setY(y);
+
+    }
+
+    public void moveLeft() {
+        
+        int newX = xCor - 1;
+        setX(newX);
+        
+    }
+    
+    public void moveRight() {
+        
+        int newX = xCor + 1;
+        setX(newX);
+        
+    }
+    
+    public void moveUp() {
+        
+        int newY = yCor + 1;
+        setY(newY);
+        
+    }
+    
+    public void moveDown() {
+        
+        int newY = yCor - 1;
+        setY(newY);
+        
+    }
+    
+    
 }
