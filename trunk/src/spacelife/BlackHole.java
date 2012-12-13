b0770a3b38667f2e0a94f853788baf670c1a3d3d
tarek.amiri <tarek.amiri@ddbea90b-85ed-395a-11f5-6ae587be2f69>
2012-12-13 14:27:30 +0000

    Implémentation de BlackHole

--- trunk/src/spacelife/BlackHole.java
+++ trunk/src/spacelife/BlackHole.java
@@ -1,16 +1,45 @@
 package spacelife;
-
+/**
+ * BlackHole
+ * WorldElement inheriting form IWorldElement Class
+ * 
+ * Implementing Singleton Pattern:
+ *  - To set the propreties of the object, you must use the initObject(...)
+ *    Method
+ * 
+ * @author Raiden
+ */
+import javax.swing.ImageIcon;
+import waterspace.ElementType;
 import waterspace.IWorldElement;
-//Silvin aime cette classe
+import waterspace.Position;
+
 public class BlackHole extends IWorldElement {
-	private BlackHole instance;
-	public BlackHole unnamed_BlackHole_;
 
-	public BlackHole getInstance() {
-		return this.instance;
-	}
+    private static BlackHole uniqueInstance;
 
-	private BlackHole() {
-		throw new UnsupportedOperationException();
-	}
+    private BlackHole() {
+
+    }
+
+    public static BlackHole getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new BlackHole();
+        }
+        return uniqueInstance;
+    }
+    
+    /**
+     * Convenience method to set the proprieties of object
+     * @param pos
+     * @param speed
+     * @param img 
+     */
+    public void initObject(Position pos, int speed, ImageIcon img){
+        this.setPos(pos);
+        this.setSpeed(speed);
+        this.setImg(img);
+        this.setType(ElementType.SPACE_BLACKHOLE);
+    }
+
 }
