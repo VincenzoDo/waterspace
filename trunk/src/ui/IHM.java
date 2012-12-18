8fe3e811580be834dd5d509b938ea2ecb1e5037e
pauline.boukhaled <pauline.boukhaled@ddbea90b-85ed-395a-11f5-6ae587be2f69>
2012-12-18 13:09:29 +0000

    - implement IHM tableau d'images. Ajout list d'elts dans le wolrdManager. - Changement WorldManager : methode getInstance public et static

--- trunk/src/ui/IHM.java
+++ trunk/src/ui/IHM.java
@@ -4,10 +4,63 @@
  */
 package ui;
 
+import java.awt.*;
+import javax.swing.*;
+import waterspace.WorldElement;
+import waterspace.WorldManager;
+
 /**
  *
  * @author PBK2
  */
-public class IHM {
+public class IHM extends JFrame{
+    // --- Components - not private to have easy access
+    JButton nextStep = new JButton("NextStep");
+    ImageIcon[][] elements = new ImageIcon[10][10];
+    private JPanel container = new JPanel();
+    private JLabel ecran = new JLabel();
+     // --- MVC
+    WorldManager vm;
+
+   
+
+    /**
+     * Constructor of the IHM
+     * <p/>
+     * @param simulation used to obtain the {@link WorldManager}, because this
+     * is the {@link Simulation} who create the {@code WorldManager}
+     */
+    public IHM(WorldManager vm) {
+        this.vm = vm;
+        this.setTitle("SimulLife");
+        JPanel panEcran = new JPanel();
+        panEcran.setPreferredSize(new Dimension(220, 30));
+        ecran.setHorizontalAlignment(JLabel.RIGHT);
+        ecran.setPreferredSize(new Dimension(220, 20));
+        // --- the main UI
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        this.setLocationRelativeTo(null);
+        
+        for(int i=0; i<vm.listElement.size() ; i++){
+            WorldElement elt = vm.listElement.get(i);
+            elements[elt.getPosition().getX()][elt.getPosition().getY()]=elt.getImage(); 
+        }
+        panEcran.add(ecran);
+        panEcran.setBorder(BorderFactory.createLineBorder(Color.black));
+        container.add(panEcran, BorderLayout.CENTER); 
+        this.setVisible(true);
+
+    }
+
+   
+    public static void main(String[] args) {
+        WorldManager world = WorldManager.getInstance();
+        IHM game = new IHM(world);
+        //Ajout de la fenêtre comme observer de notre modèle
+  }
+
+    
     
 }
